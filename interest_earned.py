345f312bcbe1987fa686fd4572316bbfd465b5c0
Ayden250 <suarez.ayden@student.ccm.edu>
2026-09-16 10:35:43 -0400

    Implement interest calculation in interest_earned.py
    Fix input prompts and calculation for interest earned.

---
+++ interest_earned.py
@@ -0,0 +1,8 @@
+amount=float(input("enter amount of interest"))
+interst_= float(input("enter interest value "))
+interst_rate=float(input("enter rate of interest"))
+time_=float(input("amount of time"))
+P= input(float("Principal #"))
+R=input(float("Rate #"))
+T=input(float)("Amount of time")
+amount=P*(1+R/T)**T
